361e2263afd16cf957333277840b446e0967dbfb
CARLOS FERNANDO ITURRA BRAVO <c.iturra07@ufromail.cl>
2025-06-12 18:43:24 -0400

    feat: admin logs and reports added

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 require("dotenv").config();
 const express = require("express");
 const connectDB = require("./src/config/db");
-
+const mongoose = require('mongoose');
 const app = express();
 
 // Conectar a la base de datos
@@ -11,10 +11,13 @@ connectDB();
 app.use(express.json());
 
 // Rutas
-app.use("/users", require("./src/routes/users.routes"));
+app.use('/api/logs', require('./src/routes/logs.routes'));
+app.use('/api/reports', require('./src/routes/reports.routes'));
 
 // Puerto
 const PORT = process.env.PORT || 4000;
 app.listen(PORT, () => {
   console.log(`Servidor escuchando en el puerto ${PORT}`);
+
+  module.exports = app;
 });
