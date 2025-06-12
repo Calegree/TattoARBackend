69ca1ec513b4c2fb2ee529da68650e6ae908d5bb
BenjaSerrano828 <iaki12373@gmail.com>
2025-06-12 17:06:43 -0400

    first commit

--- src/routes/users.routes.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const usersController = require("../controllers/users.controller");
-
-// Ruta para crear usuario
-router.post("/create", usersController.crearUsuario);
-
-module.exports = router;
