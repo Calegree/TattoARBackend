fa8720c1272858500ae21082cb4098b906f46a2f
Carlos <c.pena17@ufromail.cl>
2025-06-12 18:52:14 -0400

    fix: correct environment variable name for MongoDB connection

--- src/config/db.js
+++ src/config/db.js
@@ -2,11 +2,9 @@
 
 const mongoose = require('mongoose');
 
-const connectDB = async () => {
-  console.log(process.env.MONGO_URI);
-  
+const connectDB = async () => {  
   try {
-    await mongoose.connect(process.env.MONGO_URI, {
+    await mongoose.connect(process.env.MONGODB_URI, {
       useNewUrlParser: true,
       useUnifiedTopology: true
     });
